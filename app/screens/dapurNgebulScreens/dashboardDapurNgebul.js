62c8c45bdc562877aeef3bd14319c3cfd649f571
ignindra <ignindrakw@gmail.com>
2017-08-13 22:26:04 +0700

    Added food list into the dashboard

--- app/screens/dapurNgebulScreens/dashboardDapurNgebul.js
+++ app/screens/dapurNgebulScreens/dashboardDapurNgebul.js
@@ -3,8 +3,9 @@ import { View, Text, SectionList, StyleSheet, ToastAndroid, Dimensions, StatusBa
 import { RkTextInput, RkText } from 'react-native-ui-kitten';
 
 import { FontAwesome } from '../../assets/icons';
-import { NavBarDapurNgebul } from '../../components/index';
-import { UIConstants } from '../../config/appConstants';
+import { NavBarDapurNgebul, ShowcaseList } from '../../components/dapurNgebulComponents';
+
+let windowWidth = Dimensions.get('window').width;
 
 export class DashboardDapurNgebul extends React.Component {
   constructor(props) {
@@ -20,7 +21,7 @@ export class DashboardDapurNgebul extends React.Component {
 
   /**
    * Temporary method just to spawn a toast with changed text from the search bar.
-   * Change it when it will be used to search something.
+   * Change this when it will be used to search something.
    * @param {string} text - changed text from the text input
    */
   _filter(text) {
@@ -28,9 +29,27 @@ export class DashboardDapurNgebul extends React.Component {
     ToastAndroid.show(this.state.tempSearchBar, ToastAndroid.SHORT);
   }
 
-  render() {
+  _renderItem = function({item}) {
     return (
-      <View style={styles.headerContainer}>
+      <View>{item}</View>
+    );
+  }
+
+  _renderSection = function({section}) {
+    return (
+      <Text style={styles.sectionHeader}>{section.title}</Text>
+    );
+  }
+
+  render() {
+    const listData = [
+      {title: 'Category 1', data: [<ShowcaseList backColor={'#48a9a6'} onPressFunc={null} />]},
+      {title: 'Category 2', data: [<ShowcaseList backColor={'#731dd8'} onPressFunc={null} />]},
+      {title: 'Category 3', data: [<ShowcaseList backColor={'#c1666b'} onPressFunc={null} />]},
+    ];
+
+    return (
+      <View style={styles.container}>
         <NavBarDapurNgebul navigation={this.props.navigation} title={'Foods'} left={FontAwesome.bars} right={FontAwesome.search} />
         <View style={styles.searchBarContainer}>
           <RkTextInput
@@ -41,15 +60,23 @@ export class DashboardDapurNgebul extends React.Component {
             rkType='row'
             placeholder='Search'/>
         </View>
+        <SectionList
+          sections={listData}
+          keyExtractor={function(index) {return index}}
+          renderItem={this._renderItem}
+          renderSectionHeader={this._renderSection}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  headerContainer: {
-    width: Dimensions.get('window').width,
-    height: 60 + UIConstants.AppbarHeight
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    width: windowWidth,
+    justifyContent: 'center'
   },
   searchBarContainer: {
     backgroundColor: '#efefef',
@@ -57,5 +84,12 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     height: 60,
     alignItems: 'center'
+  },
+  sectionHeader: {
+    marginTop: 10,
+    marginLeft: 12,
+    fontSize: 20,
+    fontWeight: 'bold',
+    backgroundColor: 'white',
   }
 });
